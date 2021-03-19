9a04e93d03e4cfc8f5fefa5db53f5f7428fbbc8c
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-18 20:26:37 -0400

    commit tree ad356da91b3e9cf5a7b3a3c8255a3d87e9fbc5d7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,66 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+    def index
+      @posts = Post.all
+    end
 
-  def new
-    @post = Post.new
-  end
+    def show
+      @post = Post.find(params[:id])
+    end
 
-  # add create method here
 
-  def create
-    # @post = Post.new
-    # @post.title = params[:title]
-    # @post.description = params[:description]
-    # @post.save
-    @post = Post.create(title: params[:title], description: params[:description])
-    redirect_to post_path(@post)
-  end
+    def new
+    end
+
+    def create
+      @post = Post.new
+      @post.title = params[:title]
+      @post.description = params[:description]
+      @post.save
+      redirect_to post_path(@post)
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # def index
+  #   @posts = Post.all
+  # end
+
+  # def show
+  #   @post = Post.find(params[:id])
+  # end
+
+  # def new
+  #   @post = Post.new
+  # end
+
+  # # add create method here
+
+  # def create
+  #   # @post = Post.new
+  #   # @post.title = params[:title]
+  #   # @post.description = params[:description]
+  #   # @post.save
+  #   @post = Post.create(title: params[:title], description: params[:description])
+  #   redirect_to post_path(@post)
+  # end
 end
